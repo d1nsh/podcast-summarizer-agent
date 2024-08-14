eb4be57fb437ad8b028270aa304d8aed1769a42f
Dinesh Ramamurthy <dineshramamurthy@Dineshs-MacBook-Pro.local>
2024-08-14 00:31:30 -0700

    bug fixes

--- podcast-summarizer.py
+++ podcast-summarizer.py
@@ -5,15 +5,19 @@ from urllib.parse import urlparse
 def main():
  url = sys.argv[1]
  path  = urlparse(url)
+ prompt =f'''can you summarize the podcast at this link? {url} . 
+ Please provide a detailed summary with noteable highlights and specific information from the podcast. 
+ The summary should be well formatted so that it can read using a word processor like microsoft word. 
+ Do not ask for any feedback after the summary.'''
+
 
  if path.netloc != 'podcasts.apple.com':
   print("I currently only support apple podcasts. Please provide a valid apple podcast url")
   exit()
 
- response = ollama.generate(model='llama3', prompt= f'can you summarize the podcast at this link? {url} .  Please provide a detailed summary with noteable highlights and  specific information from the podcast. The summary should be well formatted so that it can read using a word processor like microsoft word. Do not ask for any feedback after the summary.' 
-)
+ response = ollama.generate(model='llama3', prompt=prompt)
 
- filename = path.pathreplace("/", "-").lstrip('-') + "-summary.pdf"                    
+ filename = path.path.replace("/", "-").lstrip('-') + "-summary.pdf"                    
 
  summary = response["response"]
 
